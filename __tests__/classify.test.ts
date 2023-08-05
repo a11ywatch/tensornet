3021157ff071739790fce48975597788df6f265c
j-mendez <jeff@a11ywatch.com>
2023-08-05 16:39:15 -0400

    chore(deps): fix base64 tensor tf wasm version

--- __tests__/classify.test.ts
+++ __tests__/classify.test.ts
@@ -6,22 +6,22 @@ import { classifyAsync } from "../src/classify-async";
 import { SAMPLE } from "../__mocks__/sample";
 import { ready, setBackend } from "@tensorflow/tfjs-core";
 
+
 describe("classify base64 image using tensorflow Models", () => {
-  test("use pure js", async () => {
+  beforeAll(async () => {
     await setBackend("wasm"); // set tensorflow wasm backend
     await ready();
-
+  })
+  test("use pure js", async () => {
     const classification = await classify(SAMPLE);
 
     expect(classification && classification[0].className).toEqual("Siamese cat, Siamese");
   });
 
   test.skip("use sharp", async () => {
-    await setBackend("wasm"); // set tensorflow wasm backend
-    await ready();
-
     const classification = await classifyAsync(SAMPLE);
 
+    // the async tensor returning incorrect results
     expect(classification && classification[0].className).toEqual("Siamese cat, Siamese");
   });
 });
